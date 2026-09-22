51a95d2b9b5b92e41325e5f0ecc4fd326fb4b6cf
NightCandle <instkffff@gmail.com>
2026-09-22 22:25:13 +0800

    .

--- tagSet/tagset.js
+++ tagSet/tagset.js
@@ -6,14 +6,14 @@ const name = 'admin';
 const password = '0000';
 
 /* const token = await login(baseUrl, name, password);
-console.log(token); */
+console.log(token);
+ */
+const key = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJuZXVyb24iLCJib2R5RW5jb2RlIjowLCJleHAiOjE3OTAwNTA1NjAsImlhdCI6MTc5MDA0Njk2MCwiaXNzIjoibmV1cm9uIn0.Ah2XlO7L0iy7TG8952hBS_9Vc_NQu2LcLNkVj5n_zLvb8BNxDNwDr-rHPJwVu4gqF5lywbFnoZS-FnD3Gts0KZbeq1KAKv6TzJzPHWRdGcCPNM7m4iUdTI1jX43mxSNF_iubV5OAYgPjpE2nYJbmZUwh81CZrRJ3945AnDo12DySViBfre3Tg5No7az2jCxlLKiA4plZYaQ174ylHomV8VZT-0OJEnC6mK8kltVqGhF9jpUdQXqmyCWNqIEugWnV_Ec-SoOhNFUQ0Nq-0SXmdYBtiW3CuSpRWg--8QX7iX21ZMj6yATvagv0nrCTXMVyg2fNjTA5km0ObMJNLfh5Qg';
 
-const key = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJuZXVyb24iLCJib2R5RW5jb2RlIjowLCJleHAiOjE3ODk3MzU5MTEsImlhdCI6MTc4OTczMjMxMSwiaXNzIjoibmV1cm9uIn0.VKiPOSvUXrBZlYUewiNPgPYtJraVt89yeifUxw6OhB_AkSCFzUrJyRToqhuCmNx7iB3sjaa-u6UD1dlVqhrB-AiXUu4_C0B6F2cL6clnWSP--vhSTT3YU4_ef-Lay6JjXmWTuKGEH0l1kLxiEUhQhMD2RSmBCC6e3vgYsWHIsT_Lvxcr67hPYc5ZDk2Q4XCfBZEbBe-1B2Bh1xa0Yf0PfUYT5vId4ALxc-ID-XPESatnhSMvWPmRdHQzZ9pXh4Xudx9pIw-LZ8TUuUrmUDpwBiGoJwuZYA4Le83cvm5Cth5Vc4LzDNrUg-nBGa7tv-yxpuUcL9CQLI3YOyfBecxgeQ';
-
-function tagList(unitId) {
+function tagList(unitId, offset) {
 
     let first = (unitId - 1) * 4 + 1;
-    let Id = unitId + 4;
+    let Id = unitId + offset;
 
     const tags = [
         {
@@ -95,12 +95,12 @@ function tagListConsole(unitId, num, name) {
 /* const template = await fetchTags(baseUrl, key, 'S3', 'PF5');
 console.log(template); */
 
-/* await insertTags(baseUrl, key, 'S2', 'PF4', tagList(3)); */
+await insertTags(baseUrl, key, 'S1', 'PF3', tagList(3, 5));
 
 // console
 /* const template = await fetchTags(baseUrl, key, 'console', 'PF1Switch');
 console.log(template); */
 
-await insertTags(baseUrl, key, 'console', 'VFDF', tagListConsole(14, 6, 'F'));
+/* await insertTags(baseUrl, key, 'console', 'VFDF', tagListConsole(14, 6, 'F')); */
 
 
